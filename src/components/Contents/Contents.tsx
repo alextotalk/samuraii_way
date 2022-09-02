5751721cac0c5bac80e013f093b75b6ca5aca731
alextotalk <alexoo@inbox.ru>
2022-09-02 10:56:44 +0300

    Add Redux

--- src/components/Contents/Contents.tsx
+++ src/components/Contents/Contents.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import m from "./Contents.module.css"
-import {Posts} from "./Posts/Posts";
-import {Dialogs} from "./Dialogs/Dialogs";
-import {Route} from "react-router-dom";
+
 import {ActionsTypes} from "../../Redux/State";
 import {store} from "../../Redux/reduxStore";
+import {Route, Routes} from "react-router-dom";
+import {Dialogs} from "./Dialogs/Dialogs";
+import {Posts} from "./Posts/Posts";
 
 type ContentsPropsType = {
     state: ReturnType<typeof store.getState>
@@ -14,12 +15,14 @@ type ContentsPropsType = {
 export const Contents: React.FC<ContentsPropsType> = (props) => {
     return (
         <div className={m.Contents}>
-            <Route
-                render={() => <Posts dispatch={props.dispatch} posts={props.state.profilePage.posts}
-                                     newPostText={props.state.profilePage.newPostText}/>}
-                path='/posts'></Route>
-            <Route render={() => <Dialogs dispatch={props.dispatch} dialogs={props.state.dialogsPage}/>}
-                   path='/dialogs'></Route>
+            <Routes>
+                <Route
+                    render={() => <Posts dispatch={props.dispatch} posts={props.state.profilePage.posts}
+                                         newPostText={props.state.profilePage.newPostText}/>}
+                    path='/posts'></Route>
+                <Route render={() => <Dialogs dispatch={props.dispatch} dialogs={props.state.dialogsPage}/>}
+                       path='/dialogs'></Route>
+            </Routes>
         </div>
     );
 }
--- src/components/NavBar/List.tsx
+++ src/components/NavBar/List.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import m from "./List.module.css"
-import {NavLink} from "react-router-dom";
-export function List() {
+import { NavLink} from "react-router-dom";
+ export function List() {
     return (
         <div className={m.widget}>
              <ul>
-                <li><NavLink   to="/posts" activeClassName={m.active}>Profile</NavLink></li>
+                <li><NavLink   to="/posts" className={m.active}>Profile</NavLink></li>
                 <li><NavLink   to="/dialogs">Massager</NavLink></li>
                 <li><a href="">News</a></li>
                 <li><a href="">Music</a></li>
